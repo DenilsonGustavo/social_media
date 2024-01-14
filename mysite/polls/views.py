a8f3f2c80c37722c22fbe7e1e3e89ba2617d6517
Denilson Gustavo <denilsongustavoln@gmail.com>
2024-01-14 17:20:58 -0300

    post_feed

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,18 +3,53 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile, Post
 
 
 # Create your views here.
 @login_required(login_url='signin')
 def index(request):
-    #return HttpResponse('<h1>Bem Vindo à Rede Social</h1>')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
     return render(request,'index.html')
 
 @login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+    else:
+        return redirect('/')
+    return HttpResponse('<h1>Upload View</h1>')
+
+@login_required(login_url='signin')
 def settings(request):
-    return render(request, 'setting.html')
+    user_profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        if request.FILES.get('image')==None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image')!=None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile':user_profile})
 
 def signup(request):
     if request.method == 'POST':
@@ -34,14 +69,16 @@ def signup(request):
                 user = User.objects.create_user(username=username,email=email, password=password)
                 user.save()
 
-                #log useri in and redict to settings page
+                #log user in and redict to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request,user_login)
 
                 #create a Profile object for the new user
 
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model,id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'As senhas não sao iguais!')
             return redirect('signup')
