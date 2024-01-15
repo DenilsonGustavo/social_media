33a7817389c5016b2ae540eb82940583b894c839
Denilson Gustavo <denilsongustavoln@gmail.com>
2024-01-15 13:54:48 -0300

    like_post

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,16 +3,39 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 
 
 # Create your views here.
+
 @login_required(login_url='signin')
 def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    return render(request,'index.html')
 
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts, 'user_profile': user_profile})
+
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter==None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
+        return redirect('/')
 @login_required(login_url='signin')
 def upload(request):
     if request.method == 'POST':
@@ -20,11 +43,11 @@ def upload(request):
         image = request.FILES.get('image_upload')
         caption = request.POST['caption']
 
-        new_post = Post.objects.create(user=user,image=image,caption=caption)
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
         new_post.save()
     else:
         return redirect('/')
-    return HttpResponse('<h1>Upload View</h1>')
+    return render(request, 'index.html')
 
 @login_required(login_url='signin')
 def settings(request):
